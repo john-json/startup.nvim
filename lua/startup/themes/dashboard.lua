162e2561a88066c200a5e23aa4f7f9cf3b81a9b0
John johansson <37014671+john-json@users.noreply.github.com>
2024-06-05 20:15:26 +0200

    Update dashboard.lua

--- lua/startup/themes/dashboard.lua
+++ lua/startup/themes/dashboard.lua
@@ -31,7 +31,7 @@ local settings = {
         title = "Basic Commands",
         margin = 5,
         content = {
-            { " File Browser", "Neotree ~/", " <leader>e" },
+            { " File Browser", "Neotree", " <leader>e" },
             { " New File", "lua require'startup'.new_file()", "<leader>nf" },
             { " Find File", "Telescope find_files", "<leader>ff" },
             { " Recent Files", "Telescope oldfiles", "<leader>of" },
