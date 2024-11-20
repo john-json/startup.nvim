f9db755742c7343739f235ba06e1afbf5d5c871e
John json <37014671+john-json@users.noreply.github.com>
2024-11-20 10:46:28 +0100

    Update dashboard.lua

--- lua/startup/themes/dashboard.lua
+++ lua/startup/themes/dashboard.lua
@@ -31,10 +31,12 @@ local settings = {
         title = "Basic Commands",
         margin = 5,
         content = {
-            { " File Browser", "Neotree", " <leader>e" },
-            { " New File", "lua require'startup'.new_file()", "<leader>nf" },
             { " Find File", "Telescope find_files", "<leader>ff" },
+            { "󰍉 Find Word", "Telescope live_grep", "<leader>lg" },
             { " Recent Files", "Telescope oldfiles", "<leader>of" },
+            { " File Browser", "Telescope file_browser", "<leader>fb" },
+            { " Colorschemes", "Telescope colorscheme", "<leader>cs" },
+            { " New File", "lua require'startup'.new_file()", "<leader>nf" },
 
         },
         highlight = "String",
