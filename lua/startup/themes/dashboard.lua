71029013fe659bc038d32e7d098d252e191a9ea1
John json <37014671+john-json@users.noreply.github.com>
2024-11-28 09:50:17 +0100

    Update dashboard.lua

--- lua/startup/themes/dashboard.lua
+++ lua/startup/themes/dashboard.lua
@@ -35,7 +35,7 @@ local settings = {
         margin = 5,
         content = {
             { " Find File", "Telescope find_files", "<leader>ff" },
-            { " File Explorer", "Neotree toggle", "<leader>e" },
+            { " File Explorer", "Neotree", "<leader>e" },
             { "󰍉 Find Word", "Telescope live_grep", "<leader>lg" },
             { " Recent", "Telescope oldfiles", "<leader>of" },
             { " New File", "lua require'startup'.new_file()", "<leader>nf" },
